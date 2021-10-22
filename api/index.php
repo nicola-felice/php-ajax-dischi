7e1878aebae05a44adefe99b527aaa5c1c1e4394
nicolafelice <felicenicola99@hotmail.com>
2021-10-22 15:45:00 +0200

    bonus

--- api/index.php
+++ api/index.php
@@ -4,4 +4,22 @@ require __DIR__ . "/../database.php";
 
 header("Content-Type: application/json");
 
-echo json_encode($database);
+if ( !empty($_GET["genresList"]) ) {
+  $genresList = [];
+  foreach ( $database as $elm ) {
+    if ( !in_array($elm["genre"], $genresList) ) {
+      $genresList[] = $elm["genre"];
+    }
+  };
+  echo json_encode($genresList);
+  exit();
+} 
+
+if ( !empty($_GET["genre"]) ) {
+  $arrFilteredByGenre = array_filter( $database, function($elm) {
+    return $elm["genre"] == $_GET["genre"];
+  });
+  echo json_encode($arrFilteredByGenre);
+} else {
+  echo json_encode($database);
+}
--- js/main.js
+++ js/main.js
@@ -2,14 +2,34 @@ var app = new Vue({
   el: '#app',
   data: {
     albumsList: null,
+    genresList: null,
+    genreSelected: "",
+    queries: {},
+  },
+  watch: {
+    genreSelected(val) {
+      this.queries.genre = val;
+      this.getAlbumData();
+    }
   },
   methods: {
     async getAlbumData() {
-      let res = await axios.get('http://localhost/esercizi_php/php-ajax-dischi/api/index.php');
+      let res = await axios.get('http://localhost/esercizi_php/php-ajax-dischi/api/index.php', {
+        params: this.queries
+      });
       this.albumsList = res.data;
+    },
+    async getGenresList() {
+      let res = await axios.get('http://localhost/esercizi_php/php-ajax-dischi/api/index.php', {
+        params: {
+          genresList: 1,
+        }
+      });
+      this.genresList = res.data;
     }
   },
   created() {
     this.getAlbumData();
+    this.getGenresList();
   }
 })
